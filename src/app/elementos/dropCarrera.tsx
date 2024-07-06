10c2a7d0550407580a7e6754968f9e0128990f09
jAiguiser <jhonumss@gmail.com>
2024-07-06 14:11:12 -0400

    dropdowwn

--- src/app/elementos/dropCarrera.tsx
+++ src/app/elementos/dropCarrera.tsx
@@ -12,25 +12,27 @@ const DropPrincipal: React.FC<DropPrincipalProps> = ({ carrera }) => {
 
   return (
     <li className="link-parent" >
-      <a data-dropdown className='bg-sky-400 block w-64 py-2 px-3 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500 sm:text-sm'>{nombreCarrera}</a>
+      <h1 data-dropdown className='bg-sky-400 block w-64 py-2 px-3 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500 sm:text-sm'>
+        {nombreCarrera}
+      </h1>
       <ul className="link-child">
         {semestres.map((semestre, index) => (
           <li className="link-parent " key={index}>
-            <a href="#" data-dropdown className='bg-slate-300 block w-64 py-2 px-3 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500 sm:text-sm'>
+            <h1 data-dropdown className='bg-slate-300 block w-64 py-2 px-3 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500 sm:text-sm'>
               {semestre.numberSemestre}
-            </a>
+            </h1>
             <ul className="link-child">
               {semestre.materias.map((materia, i) => (
                 <li className="link-parent" key={i}>
-                  <a href="#" data-dropdown className='bg-slate-300 block w-64 py-2 px-3 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500 sm:text-sm'>
+                  <h1 data-dropdown className='bg-slate-300 block w-64 py-2 px-3 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500 sm:text-sm'>
                     {materia.nombreMateria} 
-                  </a>
+                  </h1>
                   <ul className="link-child">
                     {materia.Docentes.map((docente) => (
                       <li className="link-parent" key={docente.nombreDocente} >
-                        <a href="#" data-dropdown>
+                        <h1 data-dropdownv>
                           {docente.nombreDocente} ({docente.aula}, {docente.horaIni} - {docente.horaFin})
-                        </a>
+                        </h1>
                       </li>
                     ))}
                   </ul>
